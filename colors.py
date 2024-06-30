100f99a1f9fabafc8d44609ece9cfc9c79eb99c5
RoundBearChoi <roundbeargames@gmail.com>
2024-07-01 02:17:26 +0900

    save

--- colors.py
+++ colors.py
@@ -1,4 +1,4 @@
-import yfinance as yf
+import cryptocompare
 import matplotlib.pyplot as plt
 import numpy as np
 import logging
@@ -6,38 +6,55 @@ import pandas as pd
 
 from matplotlib.colors import LinearSegmentedColormap
 from dateutil.relativedelta import relativedelta
-from datetime import date
+from datetime import date, timedelta
 
 
 def run():
     print('Hi..')
 
     today = date.today()
-    data = yf.download('BTC-USD', start='2015-01-01', end=today)
-    print("Today's date: " + str(today))
-    monthly_data = data.resample('ME').last().asfreq('ME')
+    num_downloads = 3  # Set the number of downloads here
+    days_per_download = 1400  # Set the number of days per download here
+
+    print('Downloading ' + str(1400) + ' days worth of data ' + str(num_downloads) + ' times..')
+    total_days = num_downloads * days_per_download
+    print('Total of ' + str(total_days) + ' days..')
+    total_years = round(total_days / 365, 2)
+    print(str(total_years) + ' years..')
+
+    data = []
+
+    for i in range(num_downloads):
+        download_date = today - timedelta(days=i*days_per_download)
+        data_part = cryptocompare.get_historical_price_day('BTC', currency='USD', limit=2000, toTs=download_date)
+        data += data_part
+
+    df = pd.DataFrame(data)
+    df['time'] = pd.to_datetime(df['time'], unit='s')
+    df.set_index('time', inplace=True, drop=False)
+    df = df.resample('ME').last().asfreq('ME')
 
     # Calculate the RSI
-    delta = monthly_data['Close'].diff()
+    delta = df['close'].diff()
     up = delta.clip(lower=0)
     down = -1*delta.clip(upper=0)
     ema_up = up.ewm(com=13, adjust=False).mean()
     ema_down = down.ewm(com=13, adjust=False).mean()
     rs = ema_up/ema_down
 
-    monthly_data['RSI'] = 100 - (100/(1 + rs))
+    df['RSI'] = 100 - (100/(1 + rs))
 
     plt.style.use('dark_background')
     plt.figure(figsize=(14, 7))
     plt.title('Monthly RSI')
 
-    log_data(monthly_data)
-    plot_rsi(monthly_data)
+    log_data(df)
+    plot_rsi(df)
 
     plt.show()
 
 
-def plot_rsi(monthly_data):
+def plot_rsi(df):
     plot_logger = logging.getLogger('plot_logger')
     plot_logger.setLevel(logging.INFO)
     plot_handler = logging.FileHandler('plot.log', mode='w')
@@ -50,9 +67,9 @@ def plot_rsi(monthly_data):
 
     norm = plt.Normalize(0, 50)
 
-    for i in np.arange(1, len(monthly_data)):
-        x_values = monthly_data.index[i - 1:i + 1]
-        y_values = monthly_data['RSI'][i - 1:i + 1]
+    for i in np.arange(1, len(df)):
+        x_values = df.index[i - 1:i + 1]
+        y_values = df['RSI'][i - 1:i + 1]
 
         months_left = None
 
@@ -75,13 +92,13 @@ def plot_rsi(monthly_data):
         plot_logger.info('')
 
 
-def log_data(monthly_data):
+def log_data(df):
     data_logger = logging.getLogger('data_logger')
     data_logger.setLevel(logging.INFO)
     data_handler = logging.FileHandler('data.log', mode='w')
     data_logger.addHandler(data_handler)
 
-    data_logger.info(monthly_data)
+    data_logger.info(df)
 
 
 if __name__ == '__main__':
